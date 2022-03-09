e4ca25d6b0014845fc274131644a60b452ea0b82
Nico Izumi <tantoyk7.com>
2022-03-10 03:59:15 +0700

    sebelum membuat tampilan wali & bk

--- index.php
+++ index.php
@@ -12,11 +12,10 @@ include("connect.php");
 	<center>
 		<img src="images/smk2.png">
 		<h1 style=" margin: 5% 25% 0% 25%; color: white; font-family: arial; opacity: 80%">Selamat Datang Di Website SMK Negeri 1 Bantul</h1>	
-
-		<a href="https://www.smkn1bantul.sch.id/"><button class="btn fourth">Website Resmi</button></a>
-		<a href="https://www.instagram.com/smkn1bantul/"><button class="btn fourth">Instagram</button></a>
-		<a href="https://www.libsys-online.xyz/smkn1bantul/opac/"><button class="btn fourth">Perpustakaan</button></a>
-		<a href="https://skansaba.id/"><button class="btn fourth">E-Learning</button></a><br>
-		<a href="home.php"><button class="btn fourth">Data Sekolah</button></a>
-		<a href="https://goo.gl/maps/2iKxqqR7MwaUFaRN7"><button class="btn fourth">Alamat</button></a>
+			<a href="https://www.smkn1bantul.sch.id/"><button class="btn fourth">Website Resmi</button></a>
+			<a href="https://www.instagram.com/smkn1bantul/"><button class="btn fourth">Instagram</button></a>
+			<a href="https://www.libsys-online.xyz/smkn1bantul/opac/"><button class="btn fourth">Perpustakaan</button></a>
+			<a href="https://skansaba.id/"><button class="btn fourth">E-Learning</button></a>
+			<a href="home.php"><button class="btn fourth">Data Sekolah</button></a>
+			<a href="https://goo.gl/maps/2iKxqqR7MwaUFaRN7"><button class="btn fourth">Alamat</button></a>
 	</center>	
